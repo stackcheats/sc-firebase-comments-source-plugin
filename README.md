11b4e71164da4a0e26b8e62f33bc7b2621c41bfe
Athiththan <code19111995@gmail.com>
2021-05-15 20:51:18 +0530

    :art: v0.1.0

--- README.md
+++ README.md
@@ -1,3 +1,73 @@
-# StackCheats Source Firebase Comments
+# Firebase Comments
+
+A Gatsby source plugin to retrieve comments from Firebase
+for StackCheats. Please find the plugin configurations and docs below.
 
 [:construction: Plugin Under Development]
+
+## Configure
+
+Add the following plugin configuration in `gatsby-config.js` to activate the plugin
+
+```js
+{
+    resolve: `stackcheats-source-firebase-comments`,
+    options: {
+        apiKey: process.env.FIREBASE_API_KEY,
+        authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+        projectId: process.env.FIREBASE_PROJECT_ID,
+        storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+        messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+        appId: process.env.FIREBASE_APP_ID,
+        measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+    },
+}
+```
+
+To communicate with Firebase, the following configurations are required
+
+```sh
+apiKey              API Key to communicate with the Firebase
+authDomain          Firebase authentication domain
+projectId           Firebase project ID
+storageBucket       Firebase storage bucket that has been configured
+messagingSenderId   Firebase messaging sender ID
+appId               Firebase application ID
+measurementId       Firebase measurement ID
+```
+
+## Firebase Collection
+
+The collection (should be) is created and named as `comments` with the following document structure
+
+```json
+{
+    "_parentId": null,
+    "author": string,
+    "slug": string,
+    "content": string,
+    "createdAt": timestamp
+}
+```
+
+## Usage
+
+Following GraphQL can be used to retrieve the comments
+
+```graphql
+allStackCheatsComments(filter: { slug: { eq: $slug } }) {
+    edges {
+        node {
+            _id
+            _parentId
+            author
+            content
+            createdAt
+        }
+    }
+}
+```
+
+## License
+
+[MIT](LICENSE)
--- gatsby-node.js
+++ gatsby-node.js
@@ -1,153 +1,95 @@
-const fs = require('fs');
-const path = require('path');
+const firebase = require('firebase/app')
+require('firebase/firestore')
 
-// exports.onPreInit = () => console.log('Loaded gatsby-plugin-firebase-comment');
+exports.sourceNodes = async (
+  { actions, createNodeId, createContentDigest },
+  pluginOptions
+) => {
+  const { createTypes, createNode } = actions
+  const {
+    apiKey,
+    authDomain,
+    projectId,
+    storageBucket,
+    messagingSenderId,
+    appId,
+    measurementId,
+  } = pluginOptions
 
-exports.sourceNodes = async ({ actions, createNodeId, createContentDigest, pluginOptions }) => {
-	const { createTypes, createNode } = actions;
-	// const { name } = pluginOptions;
+  const commentTypeDefinition = `
+      type StackCheatsComments implements Node {
+        _id: String
+			  _parentId: String
+        author: String
+        slug: String
+        content: String
+        createdAt: Date
+      }
+    `
 
-	const commentTypeDefinition = `
-        type StackCheatsComments implements Node {
-            _id: String
-            author: String
-            slug: String
-            content: String
-            createdAt: Date
-            updatedAt: Date
-        }
-    `;
+  createTypes(commentTypeDefinition)
 
-	createTypes(commentTypeDefinition);
+  let config = {
+    apiKey: apiKey,
+    authDomain: authDomain,
+    projectId: projectId,
+    storageBucket: storageBucket,
+    messagingSenderId: messagingSenderId,
+    appId: appId,
+    measurementId: measurementId,
+  }
 
-	// TODO: load the comments from the Firebase
-	const results = [
-		{
-			_id: '01',
-			author: 'Athiththan',
-			content: 'Comments are working as expected',
-			slug: '/wso2-api-manager-oauth2-protected-endpoint/content/',
-			createdAt: '02-04-2020',
-			updatedAt: '02-04-2020',
-		},
-		{
-			_id: '02',
-			author: 'Athiththan',
-			content: 'Another set of Comments are working as expected',
-			slug: '/subscribe-apis-using-store-rest-apis/content/',
-			createdAt: '02-04-2020',
-			updatedAt: '02-04-2020',
-		},
-	];
+  firebase.default.initializeApp(config)
 
-	function commentToNode(comment, { createContentDigest, createNode }) {
-		const nodeMeta = {
-			id: createNodeId(`comments-${comment._id}`),
-			parent: null,
-			children: [],
-			internal: {
-				type: `StackCheatsComments`,
-				mediaType: `text/html`,
-				content: JSON.stringify(comment),
-				contentDigest: createContentDigest(comment),
-			},
-		};
+  const commentCollection = await firebase.default
+    .firestore()
+    .collection(`comments`)
+    .get()
 
-		const node = Object.assign({}, comment, nodeMeta);
-		createNode(node);
-	}
+  function commentToNode(comment, { createContentDigest, createNode }) {
+    const nodeMeta = {
+      id: createNodeId(`comments-${comment._id}`),
+      parent: null,
+      children: [],
+      internal: {
+        type: `StackCheatsComments`,
+        mediaType: `text/plain`,
+        content: JSON.stringify(comment),
+        contentDigest: createContentDigest(comment),
+      },
+    }
 
-	results.forEach((comment) => {
-		commentToNode(comment, { createNode, createContentDigest });
-	});
-};
+    const node = Object.assign({}, comment, nodeMeta)
+    createNode(node)
+  }
+
+  commentCollection.forEach((comment) => {
+    var c = comment.data()
+    c._id = comment.id
+    c.createdAt = comment.data()['createdAt'].toDate()
+    commentToNode(c, { createNode, createContentDigest })
+  })
+}
 
 exports.createResolvers = ({ createResolvers }) => {
-	const resolver = {
-		Mdx: {
-			comments: {
-				type: ['StackCheatsComments'],
-				resolve(source, args, context, info) {
-					return context.nodeModel.runQuery({
-						query: {
-							filter: {
-								slug: { eq: source.fields.slug },
-							},
-						},
-						type: 'StackCheatsComments',
-						firstOnly: false,
-					});
-				},
-			},
-		},
-	};
+  const resolver = {
+    Mdx: {
+      comments: {
+        type: ['StackCheatsComments'],
+        resolve(source, args, context, info) {
+          return context.nodeModel.runQuery({
+            query: {
+              filter: {
+                slug: { eq: source.fields.slug },
+              },
+            },
+            type: 'StackCheatsComments',
+            firstOnly: false,
+          })
+        },
+      },
+    },
+  }
 
-	createResolvers(resolver);
-};
-
-exports.createPagesStatefully = async ({ graphql }) => {
-	const comments = await graphql(
-		`
-			{
-				allStackCheatsComments(limit: 1000) {
-					edges {
-						node {
-							_id
-							author
-							slug
-							content
-							createdAt
-						}
-					}
-				}
-			}
-		`
-	);
-
-	if (comments.errors) {
-		throw comments.errors;
-	}
-
-	const markdownPosts = await graphql(
-		`
-			{
-				allMdx(sort: { fields: [frontmatter___date], order: DESC }, limit: 1000) {
-					edges {
-						node {
-							fields {
-								slug
-							}
-						}
-					}
-				}
-			}
-		`
-	);
-
-	const posts = markdownPosts.data.allMdx.edges;
-	const _comments = comments.data.allStackCheatsComments.edges;
-
-	const commentsPublicPath = path.resolve(process.cwd(), 'public/comments');
-
-	var exists = fs.existsSync(commentsPublicPath);
-	if (!exists) {
-		fs.mkdirSync(commentsPublicPath);
-	}
-
-	posts.forEach((post) => {
-		const contentPath = post.node.fields.slug;
-		const commentsForPost = _comments
-			.filter((comment) => {
-				return comment.node.slug === contentPath;
-			})
-			.map((comment) => comment.node);
-
-		const strippedPath = contentPath
-			.split('/')
-			.filter((s) => s)
-			.join('_');
-
-		const _commentPath = path.resolve(process.cwd(), 'public/comments/', `${strippedPath}.json`);
-		fs.writeFileSync(_commentPath, JSON.stringify(commentsForPost));
-	});
-};
+  createResolvers(resolver)
+}
